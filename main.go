1a9fb6a71e42f670a57ae0a894c2df261e2b6e00
SimonAwiti <awitisimon23@gmail.com>
2023-10-31 23:08:45 +0300

    second commit

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	// this is a single line comment
-	// single line comments can be several
-	fmt.Println("Hello world ..")
+
+	fmt.Println("Getting more familia")
 }

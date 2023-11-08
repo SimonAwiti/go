b083fdea4c565b7c661f8f960a152d48c9ba4eec
SimonAwiti <awitisimon23@gmail.com>
2023-11-08 23:28:30 +0300

    conditions in go

--- main.go
+++ main.go
@@ -14,28 +14,42 @@ func main() {
 
 	var num1 int = 1 // variables can be declared by variable name type = value
 
-	var firstname string = "Simon"
+	var firstname string = "simon"
 
 	var secondname string = " " //undeclared variables are set to null
 
-	var b, f, s = true, 2.33, "lucky" //variables can also be declared in this way
+	//var b, f, s = true, 2.33, "lucky" //variables can also be declared in this way
 
-	othername := "Antonny" // with this you can declare your variables in a shorten name
-	firstname, lastname := "Simon", "Awiti"
+	//othername := "Antonny" // with this you can declare your variables in a shorten name
+	firstname, lastname := "simon", "awiti"
 
-	x, y, z := 1, 2, 3
+	//x, y, z := 1, 2, 3
 
-	sum := x + y + z
+	//sum := x + y + z
 
 	const PI = 3.142 //once set can not be used anywhere else
 
-	fmt.Println("Getting more familia")
-	fmt.Println(num1)
-	fmt.Println(firstname)
-	fmt.Println(secondname)
-	fmt.Println(b, f, s)
-	fmt.Println(othername)
-	fmt.Println(sum)
-	fmt.Println(firstname + lastname)
+	if num1 != 1 {
+		fmt.Println("not true")
+	} else if firstname != "simon" {
+		fmt.Println("not true")
+
+	} else if secondname != " " {
+		fmt.Println("not true")
+
+	} else if lastname != "awiti" {
+		fmt.Println("not true")
+	} else {
+		fmt.Println("youre not the person werelooking for")
+	}
+
+	//fmt.Println("Getting more familia")
+	//fmt.Println(num1)
+	//fmt.Println(firstname)
+	//fmt.Println(secondname)
+	//fmt.Println(b, f, s)
+	//fmt.Println(othername)
+	//fmt.Println(sum)
+	//fmt.Println(firstname + lastname)
 
 }

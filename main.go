18f49621ee2b05bafcd8f47b3eaa508a90eaece6
SimonAwiti <awitisimon23@gmail.com>
2023-11-09 23:47:35 +0300

    standard input in go

--- main.go
+++ main.go
@@ -12,44 +12,15 @@ var anyname string = "simon" // when declared out the function it can be unused
 
 func main() {
 
-	var num1 int = 1 // variables can be declared by variable name type = value
+	var name string
+	var age int
 
-	var firstname string = "simon"
-
-	var secondname string = " " //undeclared variables are set to null
-
-	//var b, f, s = true, 2.33, "lucky" //variables can also be declared in this way
-
-	//othername := "Antonny" // with this you can declare your variables in a shorten name
-	firstname, lastname := "simon", "awiti"
-
-	//x, y, z := 1, 2, 3
-
-	//sum := x + y + z
-
-	const PI = 3.142 //once set can not be used anywhere else
-
-	if num1 != 1 {
-		fmt.Println("not true")
-	} else if firstname != "simon" {
-		fmt.Println("not true")
-
-	} else if secondname != " " {
-		fmt.Println("not true")
-
-	} else if lastname != "awiti" {
-		fmt.Println("not true")
+	fmt.Print("enter your name: ")
+	fmt.Print("enter your age: ")
+	fmt.Scanf("%s %d", name, age)
+	if age != 28 {
+		fmt.Println("verification failed")
 	} else {
-		fmt.Println("youre not the person werelooking for")
+		fmt.Println("Hello simon")
 	}
-
-	//fmt.Println("Getting more familia")
-	//fmt.Println(num1)
-	//fmt.Println(firstname)
-	//fmt.Println(secondname)
-	//fmt.Println(b, f, s)
-	//fmt.Println(othername)
-	//fmt.Println(sum)
-	//fmt.Println(firstname + lastname)
-
 }

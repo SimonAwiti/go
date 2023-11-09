da584b630d5f1e2971ff2211060d060a433a3b6e
SimonAwiti <awitisimon23@gmail.com>
2023-11-10 00:07:38 +0300

    conditions with standard input

--- main.go
+++ main.go
@@ -2,25 +2,15 @@ package main
 
 import "fmt"
 
-// this is a single line comment
-
-/* multi line
-comments */
-// anyname := simon you cannot declare a var out a function like this
-
-var anyname string = "simon" // when declared out the function it can be unused
-
 func main() {
-
-	var name string
 	var age int
 
-	fmt.Print("enter your name: ")
-	fmt.Print("enter your age: ")
-	fmt.Scanf("%s %d", name, age)
+	fmt.Println("Enter your age: ")
+	fmt.Scanf("%d", &age)
+
 	if age != 28 {
 		fmt.Println("verification failed")
 	} else {
-		fmt.Println("Hello simon")
+		fmt.Println("Success")
 	}
 }

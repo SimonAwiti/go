36c3101b28ad8b4b8755146729c2f96391956603
SimonAwiti <awitisimon23@gmail.com>
2023-11-20 23:12:04 +0300

    more refresher

--- main.go
+++ main.go
@@ -3,44 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	//fmt.Print("No new")
-	//fmt.Print("in print as compared to println")
-	//fmt.Println("prints an output in a new line")
 
-	name := "Simon"
-	age := 28
-	score := 1.999923
+	firstname := "simon"
+	lastname := "awiti"
 
-	fmt.Println("mys name is ", name, " and I am ", age)
+	var defination = fmt.Sprintf("His full name is %q as firstname and %q as lastname", firstname, lastname)
 
-	// formated string printf
-	// %v is a format specifier n it can be any letter
-
-	fmt.Printf("My name is %v and I am %v yeays old \n", name, age)
-
-	//%q prints quoted strings
-
-	fmt.Printf("my name is %q \n", name)
-
-	//printing floats
-
-	fmt.Printf("Your score is %f \n", score)
-
-	//rounding off floats
-
-	fmt.Printf("Your score rounded off to the nearest two is %0.2f \n", score)
-
-	//saving formated strigs
-
-	var str = fmt.Sprintf("My name is %v and I am %v yeays old \n", name, age)
-
-	time := 2.333
-	runner := "tim"
-
-	var sentence = fmt.Sprintf("it took %0.1f for %s to complete the race", time, runner)
-
-	fmt.Println(sentence)
-
-	fmt.Println(str)
-
+	fmt.Println(defination)
 }

9ee5e0b9c3a48d2a1c455317847f918eb2b365af
SimonAwiti <awitisimon23@gmail.com>
2023-11-21 19:26:28 +0300

    arrays in go

--- main.go
+++ main.go
@@ -3,11 +3,21 @@ package main
 import "fmt"
 
 func main() {
+	//arrays
+	var onlinestores [3]string = [3]string{"aws", "target", "alibaba"}
+	var scores [3]int = [3]int{23, 22, 21}
 
-	firstname := "simon"
-	lastname := "awiti"
+	fmt.Println(scores)
 
-	var defination = fmt.Sprintf("His full name is %q as firstname and %q as lastname", firstname, lastname)
+	//another way to declare an array
 
-	fmt.Println(defination)
+	var ages = [3]int{12, 15, 17}
+
+	fmt.Println(ages, len(ages), onlinestores)
+
+	//to shorten it all
+
+	trainers := [4]string{"Josh", "Simon", "ted", "Keziah"}
+
+	fmt.Println(trainers, len(trainers))
 }

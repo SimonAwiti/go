1a25b903e0c769593c56d97c40350d48fac85139
SimonAwiti <awitisimon23@gmail.com>
2023-11-29 15:44:25 +0300

    for loops in go

--- main.go
+++ main.go
@@ -5,16 +5,13 @@ import (
 )
 
 func main() {
-	nba_teams := []string{"Lakera", "Celtics", "Mavs", "Suns"}
+	x := 0
+	for x < 10 {
+		fmt.Println("The value is: ", x)
+		x++
+	}
+	for i := 1; i < 5; i++ {
+		fmt.Println("The value is: ", i)
 
-	nba_teams = append(nba_teams, "GSW")
-
-	fev_teams := nba_teams[0:3]
-
-	nba_teams = append(nba_teams, "76sers")
-
-	statement := fmt.Sprintf("He is familiar with all the %v teams: %v, but then he like these %v teams: %v ", len(nba_teams), nba_teams, len(fev_teams), fev_teams)
-
-	fmt.Println(statement)
-
+	}
 }
